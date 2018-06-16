e9e2f4a208ba8081b48b8c4054cb1c2796965f03
mjrb <www.w.com123@gmail.com>
2018-06-16 17:52:24 -0400

    add toplist, search and tags endpoint

--- src/c1pod/mygpo.cljs
+++ src/c1pod/mygpo.cljs
@@ -1,7 +1,7 @@
 (ns c1pod.mygpo
   (:refer-clojure :exclude [get])
   (:require [cljs-http.client :as http]
-            [cljs.core.async :refer [<! go >! chan]]))
+            [cljs.core.async :refer [<! go >! chan pipe]]))
 
 (defn gen-auth [uname pass]
   "generate base64 auth string for Authentication: Basic header"
@@ -48,3 +48,25 @@
      ;;return ok? chan
      ok?
      )))
+
+(defn get-array [request-chan]
+  "returns chanel with requested array. if not 200 it puts response code on chanel"
+  (let [result (chan)]
+    (go (let [response (<! request-chan)]
+          (>! result (if (= (response :status) 200)
+                       (response :body)
+                       (throw (js/Error (str "failed to get resource: "
+                                             (response :status))))
+                       ))))
+    result))
+
+(defn get-tags [count]
+  "returns chanel with tags. if not status 200 it puts response code on chanel"
+  (get-array (noauth-get (str "/api/2/tags/" count ".json"))))
+
+(defn toplist [amount]
+  "returns chanel with top podcasts. if not status 200 it puts response code on chanel"
+  (get-array (noauth-get (str "/toplist/" amount ".json"))))
+
+(defn search [query]
+  (get-array (noauth-get (str "/search.json?q=" query))))
