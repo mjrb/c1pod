f9e26458d7efa7ca4fd9c6d2beea92bcf8eb3725
mjrb <www.w.com123@gmail.com>
2018-06-16 17:53:54 -0400

    remove circular dep and add maximize

--- src/c1pod/utils.cljs
+++ src/c1pod/utils.cljs
@@ -1,7 +1,7 @@
 (ns c1pod.utils
   (:refer-clojure :exclude [contains?])
-  (:require [c1pod.state :as state]
-            [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]
+            [cljs.core.async :refer [<! go >! chan]]))
 
 (defn feild-value [id]
   "get the value of the input with id"
@@ -20,14 +20,26 @@
     [:label label-text]
     [:br]]))
 
-(defn set-content! [element-vec]
-  (let [element (reagent/as-element element-vec)]
-    (print (. js/JSON stringify element))
-    (swap! state/app assoc :content element)))
+;; (defn set-content! [element-vec]
+;;   (let [element (reagent/as-element element-vec)]
+;;     (print (. js/JSON stringify element))
+;;     (swap! state/app assoc :content element)))
 
 (defn contains? [string other]
-  "case insensitive version of contains?"
+  "case insensitive version of contains? with regex."
   (try
     (some? (re-find (re-pattern (str "(?i)" string)) other))
-    (catch js/Error e e)
-  ))
+    (catch js/Error e )))
+(defn maximize [start func]
+  (str "used to get the max resources from a network request"
+       "given an initial amount, and a function that tames an amount and"
+       "returns an array on a channelit will attempt to use that function"
+       "and grow the collenction exponentially until it can't get any more")
+  (let [all (chan)]
+    (go (loop [amount start]
+      (let [some (<! (func amount))]
+        (if (= (count some) amount)
+          (recur (* amount 4))
+          (>! all some)
+          ))))
+    all))
