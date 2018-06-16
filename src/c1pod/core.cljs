51b41a85647339e886a6b1fc5dbe99c75bffdc04
mjrb <www.w.com123@gmail.com>
2018-06-16 17:51:34 -0400

    implement logout and forgot password. add tag search. refactor toolbar add proper search

--- src/c1pod/core.cljs
+++ src/c1pod/core.cljs
@@ -1,7 +1,7 @@
 (ns c1pod.core  
   (:require [reagent.core :as reagent :refer [atom]]
             [cljs-http.client :as http]
-            [cljs.core.async :refer [<! go]]
+            [cljs.core.async :refer [<! go go-loop]]
             [c1pod.mygpo :as mygpo]
             [c1pod.state :as state]
             [c1pod.utils :as utils]))
@@ -22,19 +22,26 @@
                                                         (utils/feild-value "pass"))}]
       [:input.btn.btn-primary
        {:value "forgot password?" :type "button"
-        :on-click #(js/alert "not yet implemented")}]
+        :on-click #(set! js/window.location.href
+                         "https://gpodder.net/register/restore_password")}]
       [:input.btn.btn-danger
        {:value "cancel" :type "button"
-        :on-click state/close-logout!}]]
+        :on-click state/close-login!}]]
      (if-not (empty? (@state/app :login-err))
        [:div.login-err.alert-danger.alert (@state/app :login-err)]
-       [:div])]
+       [:div.invisible])]
   ))
 
 (defn podcast-list-item [data]
   [:li.list-group-item
-   [:img {:src (:scaled_logo_url data)}]
+   [:img.thumbnail {:src (:scaled_logo_url data)}]
    (:title data)])
+
+(defn tag-component [tag selected]
+  [:span.tag-component.nowrap (str ":" (tag :tag) " ") [:b (tag :usage)]
+   (if selected
+     [:span "x"])
+   ])
 
 (defn search-result [title podcasts]
   (if (empty? podcasts)
@@ -44,27 +51,59 @@
           (map (fn [data] [podcast-list-item data])
                podcasts))))
 
+(defn logo []
+  [:input.btn.btn-secondary {:type "button" :value "c1pod"}])
+
+(defn searchbar []
+  [:input.form-control
+   {:type "text" :placeholder "search" :id "query"
+    :on-change #(state/search! (utils/feild-value "query"))}])
+
+(defn auth-buttons []
+  (if (@state/app :logged-in?)
+    [:span.auth-buttons.btn-group
+     [:input.btn.btn-primary
+      {:type "button" :value "logout"
+       :on-click state/logout!}]
+     [:input.btn.btn-primary
+      {:type "button" :value "subscriptions"
+       :on-click #(js/alert "not yet implemented")}]]
+    [:input.btn.btn-primary
+     {:type "button" :value "login"
+      :on-click state/show-login!}]))
+
+(defn tag-bar [relevant-tags selected-tags]
+  (if (and (empty? relevant-tags) (empty? selected-tags))
+    [:center "type for tags"]
+    (vec (concat [:center.tagbar.nowrap [:span "select tags: "]]
+            (map (fn [tag] [tag-component tag true])
+                 selected-tags)
+            (map (fn [tag] [tag-component tag false])
+                 relevant-tags)
+            ))))
+
 (defn toolbar []
-  [:span.btn-group.container-fluid.toolbar
-   [:input.btn.btn-secondary {:type "button" :value "c1pod"}]
-   [:input.form-control {:type "text" :placeholder "search" :id "query"
-                         :on-change #(state/search! (utils/feild-value "query"))}]
-   (if (@state/app :logged-in?)
-     [:input.btn.btn-primary {:type "button" :value "logout"
-                              :on-click #(js/alert "not yet implemented")}]
-     [:input.btn.btn-primary {:type "button" :value "login"
-                              :on-click state/show-login!}])
-  ])
+  [:div.toolbar
+   [:span.btn-group.container-fluid.searchbar
+    [logo] [searchbar] [auth-buttons]]
+    [tag-bar (@state/app :relevant-tags) (@state/app :selected-tags)]])
 
 (defn app []
   [:div
    [:div {:class-name (if (@state/app :show-login-box?) "blur" "noblur")}
    [toolbar]
-   [:div.container [search-result (@state/app :title) (@state/app :search-result)]]]
+    [:div.container [search-result (@state/app :title)
+                     (@state/app :search-result)]]]
    (if (@state/app :show-login-box?)
      [login-box state/app])])
 
 (state/search-top-podcasts!)
+(state/fetch-tags!)
 (reagent/render-component [app]
                           (. js/document (getElementById "app")))
 (defn on-js-reload [])
+
+(go (let [result (<! (mygpo/search "m"))]
+      (print (map #(% :subscribers) result))
+      (print (count result))))
+
