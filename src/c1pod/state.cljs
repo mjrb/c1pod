2d8c07c98be219ffc213fb305fd89a57191d8ec4
mjrb <www.w.com123@gmail.com>
2018-06-17 15:24:42 -0400

    add stopwords and handle empty queries

--- src/c1pod/state.cljs
+++ src/c1pod/state.cljs
@@ -2,7 +2,8 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [cljs.core.async :refer [<! >! chan]]
             [c1pod.mygpo :as mygpo]
-            [c1pod.utils :as utils])
+            [c1pod.utils :as utils]
+            [clojure.string :as str])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defonce app (atom {:logged-in? false
@@ -21,6 +22,7 @@
                     :tags []
                     :relevant-tags []
                     :selected-tags []
+                    :stopwords #{}
                     }))
 
 ;;this makes sure that all the requests are done loading before
@@ -74,6 +76,15 @@
      (utils/contains? query (:title item)))
    coll))
 
+(defn remove-stopwords [query]
+  "removes stopwords from a query."
+  (let [words (-> (.toLowerCase query)
+                  (.split " "))
+        good-words (filter (fn [word]
+                             (not (contains? (@app :stopwords) word)))
+                           words)]
+        (str/join " " good-words)))
+
 (defonce search-memos (atom {}))
 (defn mygpo-search-memo [query]
   "memoizes mygpo/search results to avoid some network requests"
@@ -88,18 +99,28 @@
     ))
 
 (defn query-podcasts! [query]
+  "query for podcasts on the main page"
   (set-loading! query)
-  (go (let [result (search-titles query (@app :toplist))]
+  (if (= query "")
+    (do (swap! app assoc :search-result (@app :toplist))
+        (done-loading! query))
+    (go (let [result (search-titles query (@app :toplist))
+              ;;only do the request if there's not enough results in the toplist
+              search (if (< (count result) 10)
+                       (<! (mygpo-search-memo query))
+                       nil)]
+          ;;don't show these results if we're not looking for them anymore
           (if (= query (@app :query))
             (swap! app assoc
                    :search-result
-                   (distinct (if (> (count result) 10)
-                               result
-                               (concat result (<! (mygpo-search-memo query)))))
+                   (->> (if (> (count result) 10)
+                         result
+                         (concat result search))
+                       (utils/distinct-by :url))
                    :relevant-tags
                    (take 5 (search-titles query (@app :tags))))
-                ))
-          (done-loading! query)))
+            ))
+        (done-loading! query))))
 
 (defn search-top-podcasts! []
   "change app state to search top podcasts"
@@ -111,7 +132,6 @@
                :toplist toplist
                :search-result toplist
                :query-function query-podcasts!)
-        ;;done loading
         (done-loading! "")
         )))
 
@@ -122,8 +142,17 @@
                  (sort-by :usage)
                  (reverse)))))
 
+(defn fetch-stopwords! []
+  (go (swap! app assoc :stopwords
+             (let [stopwords (-> (<! (mygpo/get-stopwords))
+                                 (:body)
+                                 (.split "\n"))]
+               (apply hash-set stopwords)))))
+
 (defn search! [query]
   (str "search the current stash for the query. this "
        "allows us to change the query behavior on the fly")
-  (swap! app assoc :query query)
-  ((@app :query-function) query))
+  (let [good-query (remove-stopwords query)]
+    (print good-query)
+    (swap! app assoc :query good-query)
+    ((@app :query-function) good-query)))
