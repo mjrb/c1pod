4cfab49fd772dd68c36baa61154c8280d6e6a5df
mjrb <www.w.com123@gmail.com>
2018-06-18 12:07:20 -0400

    add podcast rss and subscriptions

--- src/c1pod/mygpo.cljs
+++ src/c1pod/mygpo.cljs
@@ -1,8 +1,10 @@
 (ns c1pod.mygpo
   (:refer-clojure :exclude [get])
   (:require [cljs-http.client :as http]
+            [tubax.core :as xml]
+            [tubax.helpers :as xmlh]
             [cljs.core.async :refer [<! go >! chan pipe]]))
-(defonce api-root "https://www.gpodder.net")
+(defonce api-root "http://gpodder.net")
 (defonce proxy-root "https://mjwintersphp.000webhostapp.com")
 
 (defn gen-auth [uname pass]
@@ -72,3 +74,54 @@
   "retrieves list of postgres stopwords"
   (http/get (str proxy-root "/english.stop.php")
             {:with-credentials? false}))
+
+(defn get-episode [podcast-url episode-url]
+  (http/get (str api-root "/api/2/data/episode.json")
+            {:with-credentials? false
+             :query-params {:podcast (js/encodeURIComponent podcast-url)
+                            :url (js/encodeURIComponent episode-url)}}))
+
+(defn fix-keys [target-map]
+  "makes all  first-level keys in a map keywords"
+  ;; destructure key val pair and restructure
+  (into {} (map (fn [[key val]]
+                  [(keyword key) val])
+                target-map)))
+(defn get-tag [tag]
+  (-> (js/fetch (str api-root "/api/2/tag/"
+                      tag
+                      "/100.json"))
+      (.then (fn [response]
+               (if (= (.-status response) 200)
+                 (-> (.json response)
+                     (.then js->clj)
+                     (.then (partial map fix-keys))
+                     )
+                 (.-status response)
+                 )))))
+
+(defn get-podcast-rss [rss-url]
+  (let [result (chan)]
+    (go (let [ response (<! (http/get (str proxy-root "/rssget.php")
+                                      {:with-credentials? false
+                                       :query-params {:url rss-url}}))]
+          (>! result
+              (if (= (response :status) 200)
+                (xml/xml->clj (response :body))
+                (response :status)
+                ))))
+    result))
+
+(defn episodes [rss]
+  (xmlh/find-all rss {:path [:rss :channel :item]}))
+
+(defn get-subscriptions [uname auth]
+  (let [result (chan)]
+    (go (let [response (<! (get auth (str "/subscriptions/" uname ".json")))]
+          (>! result
+              (if (= (response :status) 200)
+                (response :body)
+                (response :status)
+                ))
+          ))
+    result))
